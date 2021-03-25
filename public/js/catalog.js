077fe9fb84a8979655a4109ca339729a93db5d70
Shahzod Ziyodullayev <sshahzod5@gmail.com>
2021-03-25 15:03:39 +0500

    zakaz

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -2,7 +2,7 @@ let filterName = document.querySelectorAll(".filterName")
 let filterBefore = document.querySelectorAll(".filterBefore")
 
 filterName.forEach(item => item.addEventListener("click", () => {
-    filterBefore.forEach(e => e.style.height = "100px")
+    filterBefore.forEach.classList.toggle("autoWidth")
 }))
 
 console.log(filterBefore)
